6fa36f5a53586d571d75613807cdcdde7d9a3b29
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-02-28 01:01:16 -0500

    partially finished with actions and reducers

--- frontend/src/actions/UserActions.js
+++ frontend/src/actions/UserActions.js
@@ -1,26 +1,55 @@
 import { axiosWithAuth } from '../utils/axiosWithAuth'
 
-export const ERROR = "ERROR"
+export const REGISTER = "REGISTER"
 export const LOGIN = "LOGIN"
+export const LOGOUT = "LOGOUT"
 export const ADD_ARTICLE = "ADD_ARTICLE"
 export const UPDATE_ARTICLE = "UPDATE_ARTICLE"
-export const LOGGIN_IN = "LOGGIN_IN"
-export const SET_USER = "SET_USER"
 export const DELETE_ARTICLE = "DELETE_ARTICLE"
-export const LOGOUT = "LOGOUT"
+
 
 const URL = ""
 
-export const login = (user, fn) => dispatch => {
+export const register = (userRegister) => dispatch => {
+    axiosWithAuth()
+        .post('/register', userRegister)
+        .then(res => {
+            console.log(res)
+            dispatch({ type: REGISTER, payload: res })
+            window.localStorage.setItem("token", res)
+        })
+}
+
+export const login = (user) => dispatch => {
     axiosWithAuth()
         .post('/login', user)
         .then(res => {
             console.log(res.data.user.id)
             dispatch({ type: LOGIN, payload: res.data.user.id })
             window.localStorage.setITem("token", res.data.token)
-            fn(`/user/${res.data.user.id}`)
         })
         .catch(err => {
             console.log("error")
         })
+}
+
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT })
+    window.localStorage.removeItem("token")
+}
+
+export const addArticle = article => dispatch => {
+    console.log("articles from actions -> updateArticles(articles)", article)
+    dispatch({ type: ADD_ARTICLE, payload: article })
+}
+
+export const updateArticle = article => dispatch => {
+    console.log("articles from actions -> updateArticles(articles)", article)
+    dispatch({ type: UPDATE_ARTICLE, payload: article })
+}
+
+export const deleteArticle = (userId, articleId) => dispatch => {
+    axiosWithAuth()
+        .delete(`/user/${userId}/articles/${articleId}`)
+    dispatch({ type: DELETE_ARTICLE, payload: userId })
 }
--- frontend/src/reducers/UserReducers.js
+++ frontend/src/reducers/UserReducers.js
@@ -1,3 +1,5 @@
+import * as actionTypes from '../actions/UserActions'
+
 const initialState = {
     loggedIn: false,
     logginIn: false,
@@ -9,6 +11,27 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case 
+        case actionTypes.LOGIN:
+            return {
+                ...state,
+                loggedIn: true,
+                id: action.payload
+            }
+
+        case actionTypes.REGISTER:
+            return {
+                ...state,
+                username: action.payload.username,
+                email: action.payload.email,
+                password: action.payload.password
+            }
+
+        case actionTypes.ADD_ARTICLE:
+            return {
+                ...state,
+                articles: [...state.articles, action.payload]
+            }
+        default:
+            return state
     }
 }
